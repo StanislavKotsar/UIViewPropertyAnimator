3696b202d200c0a364f57ba55cc229c7faa1cd26
Stanislav Kotsar <minolin27@gmail.com>
2019-10-06 22:33:51 +0300

    add keyframe animation

--- LockSearch/AnimatorFactory.swift
+++ LockSearch/AnimatorFactory.swift
@@ -25,4 +25,32 @@ class AnimatorFactory {
     return scale
   }
   
+   @discardableResult static func jiggle(view: UIView) -> UIViewPropertyAnimator {
+    return UIViewPropertyAnimator.runningPropertyAnimator(
+      withDuration: 0.33, delay: 0, animations: {
+        UIView.animateKeyframes(withDuration: 1, delay: 0,
+          animations: {
+
+            UIView.addKeyframe(withRelativeStartTime: 0.0,
+              relativeDuration: 0.25) {
+              view.transform = CGAffineTransform(rotationAngle: -.pi/8)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.25,
+              relativeDuration: 0.75) {
+              view.transform = CGAffineTransform(rotationAngle: +.pi/8)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.75,
+              relativeDuration: 1.0) {
+              view.transform = CGAffineTransform.identity
+            }
+          },
+          completion: nil
+        )
+      },
+      completion: {_ in
+        view.transform = .identity
+      }
+    )
+  }
+  
 }
